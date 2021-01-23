c1dfbe2a372e7c304240c25871881422e1b663ae
Julia Lu Dac <julialudac@yahoo.fr>
2021-01-23 20:39:45 +0100

    Implement the get the tier list and add item to tier using fake database

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require('express');
+
+
+const app = express();
+app.use(require('body-parser').json());
+
+// false db
+let fake_tierlist = {
+    'S': [],
+    'A': [],
+    'B': [],
+    'C': [],
+    'D': ['Caca au pot'],
+    'E': [],
+    'F': [],
+};
+
+app.post('/tierlist/add', (req, res) => {
+    const rank = req.body.rank;
+    if (rank in fake_tierlist) {
+        fake_tierlist[rank].push(req.body.item);
+        res.status(201).send();
+    } else {
+        res.status(400)
+        .send(`Error: Rank must be one of the following: 
+              ${Object.keys(fake_tierlist)}.`)
+    }
+    // TODO: handle all the possible bad cases: 
+    // - reject the request if the item already exists in the db.
+    // - reject the request that doesnt have rank and item
+    // ...
+});
+
+app.get('/tierlist', (req, res) => {
+    res.send(fake_tierlist);
+});
+
+app.listen(5000);
