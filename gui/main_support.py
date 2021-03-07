8be9a5802d41542a3018cc358fe4fd529c91de02
Julia Lu Dac <julialudac@yahoo.fr>
2021-03-07 19:01:29 +0100

    Implement feature add item

--- gui/main_support.py
+++ gui/main_support.py
@@ -8,7 +8,8 @@ class MainSupport(wx.Frame):
     def __init__(self):
         super().__init__(None, title="Tierlist", size=(800, 300))
         self.__draw_menu__()
-        self.item_to_delete = None
+        self.selected_item = None
+        self.selected_tier = None
         self.tierlist_manipulator = TierlistManipulator("tierlist")
 
     def __display_menu_page__(self, event):
@@ -21,14 +22,20 @@ class MainSupport(wx.Frame):
 
     def __display_remove_item_page__(self, event):
         self.__clear__()
-        self.__draw_remove_item_field()
+        self.__draw_remove_item_field__()
+        self.__draw_back_to_menu__()
+
+    def __display_add_item_page__(self, event):
+        self.__clear__()
+        self.__draw_add_item_widgets__()
         self.__draw_back_to_menu__()
 
     def __draw_menu__(self):
         self.__clear__()
         read_tierlist_button = wx.Button(self, 1, "Get tierlist", pos=(100, 100))
         read_tierlist_button.Bind(wx.EVT_BUTTON, self.__display_tierlist_page__)
-        wx.Button(self, 2, "Add item to tier", pos=(100, 125))
+        add_item_button = wx.Button(self, 2, "Add item to tier", pos=(100, 125))
+        add_item_button.Bind(wx.EVT_BUTTON, self.__display_add_item_page__)
         remove_item_button = wx.Button(self, 3, "Remove item", pos=(100, 150))
         remove_item_button.Bind(wx.EVT_BUTTON, self.__display_remove_item_page__)
 
@@ -42,24 +49,43 @@ class MainSupport(wx.Frame):
         back_button.Bind(wx.EVT_BUTTON, self.__display_menu_page__)
 
     # TODO
-    def __draw_add_item_widgets(self):
-        pass
+    def __draw_add_item_widgets__(self):
+        wx.StaticText(self, label="Name of the item to add:", pos=(10, 0))
+        entered_item_field = wx.TextCtrl(self, 5, "", (10, 30))
+        entered_item_field.Bind(wx.EVT_TEXT, self.__update_selected_item__)
+        wx.StaticText(self, label="Tier of the item to add:", pos=(10, 50))
+        tier_selection_list = wx.ListBox(self, pos=(10, 70), choices=["S", "A", "B", "C", "D", "E", "F"])
+        tier_selection_list.Bind(wx.EVT_LISTBOX, self.__update_selected_rank)
+        add_item_button = wx.Button(self, label="Add item!", pos=(200, 50))
+        add_item_button.Bind(wx.EVT_BUTTON, self.__add_item_to_tierlist__)
 
-    def __draw_remove_item_field(self):
+    def __draw_remove_item_field__(self):
         wx.StaticText(self, label="Please select item to remove:", pos=(0, 0))
         choices = self.tierlist_manipulator.get_all_items()
         delete_selection = wx.ListBox(self, size=(100, -1), pos=(0, 20), choices=choices)
-        delete_selection.Bind(wx.EVT_LISTBOX, self.__update_item_to_delete__)
+        delete_selection.Bind(wx.EVT_LISTBOX, self.__update_selected_item__)
         remove_button = wx.Button(self, 2, "Remove!", pos=(120, 150))
         remove_button.Bind(wx.EVT_BUTTON, self.__remove__tierlist__item)
 
-    def __update_item_to_delete__(self, event):
-        self.item_to_delete = event.GetEventObject().GetStringSelection()
+    def __update_selected_item__(self, event):
+        # Case when we select the item (during delete action)
+        self.selected_item = event.GetEventObject().GetStringSelection()
+        # Case when we write the item (during add action)
+        if not self.selected_item:
+            self.selected_item = event.GetEventObject().GetValue()
+
+    def __update_selected_rank(self, event):
+        self.selected_tier = event.GetEventObject().GetStringSelection()
+
+    def __add_item_to_tierlist__(self, event):
+        print("Item to be added:", self.selected_item, "and selected rank:", self.selected_tier)
+        self.tierlist_manipulator.add(self.selected_item,
+                                      self.selected_tier)
 
     def __remove__tierlist__item(self, event):
-        if self.item_to_delete:
-            print(self.item_to_delete, "deleted!!!")
-            self.tierlist_manipulator.remove(self.item_to_delete)
+        if self.selected_item:
+            print(self.selected_item, "deleted!!!")
+            self.tierlist_manipulator.remove(self.selected_item)
         choices = self.tierlist_manipulator.get_all_items()
         listbox = self.GetChildren()[1]
         listbox.Set(choices)
--- gui/tierlist_manipulator.py
+++ gui/tierlist_manipulator.py
@@ -15,6 +15,16 @@ class TierlistManipulator:
         tierlist = self.load_tier_list()
         return [item for items in tierlist.values() for item in items]
 
+    def add(self, item, rank):
+        url = "http://localhost:5000/tierlist/add"
+        data = {
+            'rank': rank,
+            'item': item,
+            'tierlist_name': self.tierlist_name
+        }
+        response = requests.post(url, json=data)
+        print("Add operation status:", response.status_code)
+
     def remove(self, item):
         url = "http://localhost:5000/tierlist/del/{}/{}".format(self.tierlist_name, item)
         response = requests.delete(url)
